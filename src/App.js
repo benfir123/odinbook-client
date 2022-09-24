4b35d0737e867ff9f1fa57925daa3ce548e1cb2b
Ben Chanapai <ben_chanapai04@hotmail.com>
2022-09-24 13:45:13 +0700

    Add basename to HashRouter

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
   return (
     <div className="App">
       <Nav user={user} setUser={setUser} />
-      <HashRouter>
+      <HashRouter basename="/odinbook-client">
         <Routes>
           <Route
             path="/signin"
